2059894b0701785b534a2e306ca1e9ed7ba4c688
Luke Bellamy <lukebellamy053@gmail.com>
2019-07-31 14:21:08 +0100

    zip artifacts

--- test/Utils/PathHandler.spec.ts
+++ test/Utils/PathHandler.spec.ts
@@ -29,15 +29,13 @@ describe('PathHandler', function() {
     describe('RegisterProxy', function() {
         let serverObject: CustomServer;
 
-        before(done => {
-            let isDone = false;
-            chai.use(chaiHttp);
-            serverObject = new CustomServer({ PORT: 8081, APP_BUILD: 1, APP_VERSION: '1', SERVICE_NAME: 'Test' });
-            ExpressServer.events.on(ServerEvents.SERVER_READY, () => {
-                if (!isDone) {
-                    done();
-                    isDone = true;
-                }
+        before(() => {
+            return new Promise(resolve => {
+                chai.use(chaiHttp);
+                serverObject = new CustomServer({ PORT: 8081, APP_BUILD: 1, APP_VERSION: '1', SERVICE_NAME: 'Test' });
+                ExpressServer.events.on(ServerEvents.SERVER_READY, () => {
+                    resolve();
+                });
             });
         });
 
