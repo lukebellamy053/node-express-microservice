4d47f1fea56c6a7fbec2fe21d48e42cee3903e7a
Luke Bellamy <lukebellamy053@gmail.com>
2019-08-03 15:27:05 +0100

    Small test updates

--- src/Decorators/Timeout.ts
+++ src/Decorators/Timeout.ts
@@ -3,17 +3,8 @@ import { Controller } from '../Server';
 /**
  * Add a timeout limit to a method, default is 10 seconds
  * @param limit
- * @deprecated use Timeout instead
  */
-export function timeout(limit: number = 10 * 1000) {
-    return Timeout(limit);
-}
-
-/**
- * Add a timeout limit to a method, default is 10 seconds
- * @param limit
- */
-export function Timeout(limit: number = 10 * 1000) {
+export function Timeout(limit: number) {
     return function(target: any, propertyKey: string) {
         const method = target.constructor.name + '@' + propertyKey;
         Controller.addTimeout(method, limit);
